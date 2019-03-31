b4643e962900b0cd78ca1dc6878ffca20b0b3e33
kolesan <kolesan90@gmail.com>
2019-03-31 14:00:36 +0300

    Add a function that returns last year commit count grouped by week for a repo. Rename ContributorCountProvider to RepoInfoProvider.

---
+++ src/services/RepoInfoProvider.test.integration.ts
@@ -0,0 +1,35 @@
+import { contributorCount, commitStats } from './RepoInfoProvider';
+import log from '../utils/Logging';
+
+describe('"contributorCount" provides the number of contributors for a repo with', () => {
+
+  test(`1 page of contributors (no link header)`, async () => {
+    const count = await contributorCount("kolesan", "repo-explorer");
+    expect(count).toEqual(1);
+  });
+
+  test(`2 pages of contributors`, async () => {
+    const count = await contributorCount("jest-community", "vscode-jest");
+    expect(count).toEqual(32);
+  });
+
+  test(`>2 pages of contributors`, async () => {
+    const count = await contributorCount("axios", "axios");
+    expect(count).toEqual(159);
+  });
+
+});
+
+describe('"commitStats" provides last year participation statistics for a repo', () => {
+
+  test(`returns a 52 elements long number array`, async () => {
+    const participation = await commitStats("kolesan", "repo-explorer");
+    // log(participation);
+
+    expect(participation).toHaveLength(52);
+    expect(participation).toEqual([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8
+    ]);
+  });
+
+});
