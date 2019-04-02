4ab70bdf860721ba9d1ac71e73bf8d5e81673385
kolesan <kolesan90@gmail.com>
2019-04-02 16:27:43 +0300

    Simplify contributor count resolvment. Use per_page=1 query param to reduce number of needed requests from 2 to 1.

--- src/apis/v3/GitHubApiV3.test.integration.ts
+++ src/apis/v3/GitHubApiV3.test.integration.ts
@@ -5,19 +5,14 @@ jest.setTimeout(30000);
 
 describe('"contributorCount" provides the number of contributors for a repo with', () => {
 
-  test(`1 page of contributors (no link header)`, async () => {
+  test(`1 contributor`, async () => {
     const count = await contributorCount("kolesan", "repo-explorer");
     expect(count).toEqual(1);
   });
 
-  test(`2 pages of contributors`, async () => {
-    const count = await contributorCount("jest-community", "vscode-jest");
-    expect(count).toEqual(32);
-  });
-
-  test(`>2 pages of contributors`, async () => {
+  test(`Lots of contributors`, async () => {
     const count = await contributorCount("axios", "axios");
-    expect(count).toEqual(159);
+    expect(count).toEqual(174);
   });
 
 });
--- src/apis/v3/GitHubApiV3.ts
+++ src/apis/v3/GitHubApiV3.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import parseLinkHeader from 'parse-link-header';
 import token from '../../token';
 
-const PER_PAGE = 30;
-
 const client = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
@@ -11,18 +9,17 @@ const client = axios.create({
   }
 })
 
+//Due to inconsistency in github api this number will often be bigger than the one seen on the web page
+//https://github.community/t5/GitHub-API-Development-and/API-Github-list-contributors/m-p/9737/highlight/true#M6
 export async function contributorCount(owner: string, repo: string): Promise<number> {
-  const firstPageResponse = await client.get(`/repos/${owner}/${repo}/contributors`);
+  const firstPageResponse = await client.get(`/repos/${owner}/${repo}/contributors?per_page=1&anon=true`);
   const links = parseLinkHeader(firstPageResponse.headers.link);
 
   if (links) {
-    const lastLink = links.last;
-    const lastPageResponse = await client.get(lastLink.url);
-    const contributorCountBeforeLastPage = (parseInt(lastLink.page) - 1) * PER_PAGE
-    return contributorCountBeforeLastPage + lastPageResponse.data.length;
+    return parseInt(links.last.page);
   }
 
-  return firstPageResponse.data.length;
+  return 1;
 }
 
 export async function commitStats(owner: string, repo: string): Promise<number[]> {
