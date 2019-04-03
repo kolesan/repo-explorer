279037f6496829c83851a5fd227919a30db11183
kolesan <kolesan90@gmail.com>
2019-04-03 15:35:22 +0300

    Add 'sequential' promise utility that lets you enqueue promise providers that create promises from previous promise results.

--- src/utils/PromiseUtils.test.ts
+++ src/utils/PromiseUtils.test.ts
@@ -1,4 +1,4 @@
-import { onlyLast, allInOrder } from "./PromiseUtils";
+import { onlyLast, allInOrder, sequential, sequenceStartingValue } from "./PromiseUtils";
 
 jest.useRealTimers();
 
@@ -14,7 +14,7 @@ test(`'onlyLast' lets you stack promises and only perform the last one`, async (
   expect(someVariable).toBe(4);
 });
 
-test(`'allInOrder' lets you stack promises and only perform the last one`, async () => {
+test(`'allInOrder' lets you enque promises and guarantees that 'then' will be called with their results in the order of stacking`, async () => {
   let results: number[] = [];
   let toQueue = allInOrder((result: number) => results.push(result));
 
@@ -24,6 +24,26 @@ test(`'allInOrder' lets you stack promises and only perform the last one`, async
   await toQueue(Promise.resolve(4));
 
   expect(results).toEqual([1, 2, 3, 4]);
+});
+
+test(`'sequential' lets you enqueue functions that produce promises using the results of previous promises`, async () => {
+  let results: number[] = [];
+  const globalThen = (result?: any) => results.push(result);
+  let toSequence = sequential(globalThen);
+
+  const delayedPromiseProvider = (delay: number) => {
+    return async (result: any) => {
+      await delayedPromise(delay);
+      return result === sequenceStartingValue ? 0 : result + 1;
+    }
+  }
+
+  toSequence(delayedPromiseProvider(300));
+  toSequence(delayedPromiseProvider(100));
+  toSequence(delayedPromiseProvider(200));
+  await toSequence(delayedPromiseProvider(0));
+
+  expect(results).toEqual([0, 1, 2, 3]);
 });
 
 function delayedPromise(timeout: number): Promise<any> {
--- src/utils/PromiseUtils.ts
+++ src/utils/PromiseUtils.ts
@@ -15,3 +15,22 @@ export function allInOrder(then: Function) {
     return lastPromise = lastPromise.then(() => promise.then(result => then(result)));
   }
 }
+
+export const sequenceStartingValue = Symbol("Special value that needs to be ignored during sequential promise processing");
+export function sequential(then: (val: any) => void) {
+  let lastPromise = Promise.resolve(sequenceStartingValue);
+
+  return function toSequence(promiseProvider: (val: any) => Promise<any>) {
+    return lastPromise = lastPromise.then(nextPromise);
+
+    function nextPromise(lastPromiseResult: any) {
+      return promiseProvider(lastPromiseResult).then(acceptResult);
+    }
+  }
+
+  function acceptResult(result: any) {
+    then(result);
+    return result;
+  }
+  
+}
