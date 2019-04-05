5801d27e3792b9bf3195af6c9822165aef117650
kolesan <kolesan90@gmail.com>
2019-04-05 20:50:59 +0300

    Fix balsamiq panel props

--- src/components/balsamiq/panel/BalsamiqPanel.tsx
+++ src/components/balsamiq/panel/BalsamiqPanel.tsx
@@ -1,15 +1,10 @@
 import './balsamiq_panel.css';
 import React from 'react';
 
-interface BalsamiqPanelProps {
-  readonly className?: string;
-  readonly children?: any;
-}
-
-export default function BalsamiqPanel(props: BalsamiqPanelProps) {
-  const { className } = props;
+export default function BalsamiqPanel(props: any) {
+  const { className, ...otherProps } = props;
   return (
-    <div className={"balsamiq_panel " + (className ? className : "")}>
+    <div className={"balsamiq_panel " + (className ? className : "")} {...otherProps}>
       {props.children}
     </div>
   );
